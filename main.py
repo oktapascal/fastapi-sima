37536e40f6d5027d596d77eedef407bed5597fd0
oktapascal <oktaiscool@gmail.com>
2024-08-06 14:49:31 +0700

    update type y

--- main.py
+++ main.py
@@ -1290,7 +1290,7 @@ def import_excel_performance_data_cost(background_task: BackgroundTasks, file: U
 class BaseRentBatch(BaseModel):
     kode_aset: str
     var_model: float
-    y: List[int]
+    y: List[float]
     x: List[float]
 
 
